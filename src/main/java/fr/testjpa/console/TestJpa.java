97acce47716c8931997b573e755a66a22a6be0cc
hmerciol <li.henri@merciol.fr>
2018-01-15 16:15:20 +0100

    meilleur usage des fonctionnalitées hibernate

--- src/main/java/fr/testjpa/console/TestJpa.java
+++ src/main/java/fr/testjpa/console/TestJpa.java
@@ -44,8 +44,8 @@ public class TestJpa {
 
 		// trouver un livre avec son titre
 		String bookTitle = "Germinal"; // titre du livre
-		TypedQuery<Livre> query1 = manager.createQuery("select l from Livre l where l.titre='" + bookTitle + "'",
-				Livre.class);
+		TypedQuery<Livre> query1 = manager.createQuery("select l from Livre l where l.titre=:title", Livre.class);
+		query1.setParameter("title", bookTitle);
 		LOG.info("livres trouvés pour le titre " + bookTitle + " :");
 		for (Livre livre2 : query1.getResultList()) {
 			LOG.info("livre d'identifiant " + livre2.getId() + ", de titre " + livre2.getTitre() + " par "
@@ -55,11 +55,9 @@ public class TestJpa {
 		// trouver un emprunt et les livres associés
 		int empruntId = 1; // id de l'emprunt
 		Emprunt emprunt1 = manager.find(Emprunt.class, empruntId);
-		TypedQuery<Livre> query2 = manager
-				.createQuery("select l from Livre l join l.emprunts e where e.id=" + emprunt1.getId(), Livre.class);
 		LOG.info("livres concernés par l'emprunt " + emprunt1.getId() + " du " + emprunt1.getDateDebut().toString()
 				+ " au " + emprunt1.getDateFin().toString() + " :");
-		for (Livre livre3 : query2.getResultList()) {
+		for (Livre livre3 : emprunt1.getLivres()) {
 			LOG.info("livre d'identifiant " + livre3.getId() + ", de titre " + livre3.getTitre() + " par "
 					+ livre3.getAuteur());
 		}
@@ -67,10 +65,8 @@ public class TestJpa {
 		// trouver tous les emprunts d'un client
 		int clientId = 2; // id du client
 		Client client1 = manager.find(Client.class, clientId);
-		TypedQuery<Emprunt> query3 = manager
-				.createQuery("select e from Emprunt e join e.client c where c.id=" + client1.getId(), Emprunt.class);
 		LOG.info("Emprunts effectués par " + client1.getPrenom() + " " + client1.getNom() + " :");
-		for (Emprunt emprunt2 : query3.getResultList()) {
+		for (Emprunt emprunt2 : client1.getEmprunts()) {
 			LOG.info("Emprunt n°" + emprunt2.getId() + " du " + emprunt2.getDateDebut().toString() + " au "
 					+ emprunt2.getDateFin().toString());
 		}
