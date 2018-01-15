6aa57ddc02bf32f0ddc74e77450adc72866458f1
hmerciol <li.henri@merciol.fr>
2018-01-15 12:01:32 +0100

    tp 2, entité livre

--- src/main/java/fr/testjpa/console/TestJpa.java
+++ src/main/java/fr/testjpa/console/TestJpa.java
@@ -1,3 +1,5 @@
+package fr.testjpa.console;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -5,14 +7,38 @@ import javax.persistence.Persistence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.testjpa.model.Livre;
+
+/**
+ * @author hmerciol
+ *
+ */
 public class TestJpa {
-	
+
+	/**
+	 * Logger pour l'application
+	 */
 	private static final Logger LOG = LoggerFactory.getLogger(TestJpa.class);
 
+	/**
+	 * Point d'entrée de l'application
+	 * 
+	 * @param args
+	 *            Paramètres
+	 */
 	public static void main(String[] args) {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("pu_essai");
 		EntityManager manager = factory.createEntityManager();
-		
+
+		int bookId = 3;
+
+		Livre livre = manager.find(Livre.class, bookId);
+		if (livre != null) {
+			LOG.info("livre d'identifiant " + bookId + " : " + livre.getTitre() + " par " + livre.getAuteur());
+		} else {
+			LOG.info("Livre non trouvé");
+		}
+
 		manager.close();
 		factory.close();
 	}
---
+++ src/main/java/fr/testjpa/model/Livre.java
@@ -0,0 +1,57 @@
+package fr.testjpa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Représentation d'un livre (liée à la table du même nom)
+ * 
+ * @author hmerciol
+ *
+ */
+@Entity
+@Table(name = "LIVRE")
+public class Livre {
+
+	/**
+	 * Identifiant du livre
+	 */
+	@Id
+	private Integer id;
+
+	/**
+	 * Titre du livre
+	 */
+	private String titre;
+
+	/**
+	 * Auteur du livre
+	 */
+	private String auteur;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+}
