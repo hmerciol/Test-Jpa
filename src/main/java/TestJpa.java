9c0396e001b70cbbc604dcb7fb4d217ba806d526
hmerciol <li.henri@merciol.fr>
2018-01-15 11:15:12 +0100

    initial commit

---
+++ src/main/java/TestJpa.java
@@ -0,0 +1,20 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestJpa {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(TestJpa.class);
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("pu_essai");
+		EntityManager manager = factory.createEntityManager();
+		
+		manager.close();
+		factory.close();
+	}
+
+}
